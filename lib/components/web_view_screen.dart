763293ab2872384e1a4301f434e36bc420073965
Yusei Nabana <20im0135@iu.ac.jp>
2023-03-13 15:18:11 +0900

    webviewを３系に変更

--- lib/components/web_view_screen.dart
+++ lib/components/web_view_screen.dart
@@ -1,81 +1,36 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// Androidに対応
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
 class WebViewPage extends StatefulWidget {
   const WebViewPage({Key? key, required this.urlString}) : super(key: key);
   final String urlString;
 
-
   @override
-  _WebViewPageState createState() => _WebViewPageState();
+  State<WebViewPage> createState() => _WebViewPageState();
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  late final WebViewController _controller;
-
+  late WebViewController? _controller;
 
   bool _isLoading = false;
   double? _newHeight;
 
-
   @override
   void initState() {
     super.initState();
-
-    // #docregion platform_features
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    final WebViewController controller =
-    WebViewController.fromPlatformCreationParams(params);
-
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              _isLoading = true;
-            });
-          },
-          onPageFinished: (String url) async {
-            final newHeight = await _getNewHeight();
-            setState(() {
-              _isLoading = false;
-              _newHeight = newHeight;
-            });
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(widget.urlString));
-
-
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
-
-    _controller = controller;
-
+    // if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
+
 
   Future<double> _getNewHeight() async {
     const String javaScript = 'document.documentElement.scrollHeight;';
-    final result = await _controller.runJavaScriptReturningResult(javaScript);
+    final result = await _controller?.runJavascriptReturningResult(javaScript);
     final getHeight = double.tryParse(result.toString()) ?? 0;
     return getHeight;
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -84,16 +39,39 @@ class _WebViewPageState extends State<WebViewPage> {
         children: [
           if (_isLoading) const LinearProgressIndicator(),
           Expanded(
-              child: SingleChildScrollView(
-                physics: const ClampingScrollPhysics(),
-                child: SizedBox(
-                  height: _newHeight ?? MediaQuery.of(context).size.height * 0.9,
-                  child: WebViewWidget(
-                      controller: _controller
-                  ),
+            child: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: SizedBox(
+                height: _newHeight ?? MediaQuery.of(context).size.height * 0.9,
+                child: WebView(
+                  initialUrl: widget.urlString,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  // jsを有効化
+                  onWebViewCreated: (controller) async {
+                    _controller = controller;
+                  },
+                  // controller紐付け
+                  onPageStarted: (String url) {
+                    // 読込開始時
+                    setState(() {
+                      _isLoading = true;
+                    });
+                  },
+                  onPageFinished: (String url) async {
+                    Future.delayed(const Duration(milliseconds: 2000));
+                    print("2s経過！！");
+                    final newHeight = await _getNewHeight();
+                    setState(() {
+                      _isLoading = false;
+                      _newHeight = newHeight;
+                      print("高さを更新！！");
+                      print(_newHeight);
+                    });
+                  },
                 ),
-              )
-          ),
+              ),
+            ),
+          )
         ],
       ),
     );
