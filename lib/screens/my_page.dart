6eecd116890c73df0712215fdb005f1311c0f9f1
Yusei Nabana <20im0135@iu.ac.jp>
2023-03-29 23:30:26 +0900

    マイページ（未ログイン時）のアイコンを削除

--- lib/screens/my_page.dart
+++ lib/screens/my_page.dart
@@ -37,52 +37,49 @@ class _MyPageState extends State<MyPage> {
     } else {
       // TODO ログインしていない場合に表示するように設定する
       return Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 227),
-          child: Column(
-            children: [
-              const Icon(Icons.login, size: 48),
-              const Text(
-                'ログインが必要です',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF333333),
-                  letterSpacing: -0.24,
-                  height: 1.7,
-                ),
+        child: Column(
+          children: [
+            SizedBox(height: deviceHeight * 0.267,),
+            const Text(
+              'ログインが必要です',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF333333),
+                letterSpacing: -0.24,
+                height: 1.7,
               ),
-              const SizedBox(height: 6),
-              const Text(
-                'マイページの機能を利用するには\nログインを行っていただく必要があります。',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF828282),
-                  height: 2,
-                ),
+            ),
+            const SizedBox(height: 6),
+            const Text(
+              'マイページの機能を利用するには\nログインを行っていただく必要があります。',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF828282),
+                height: 2,
               ),
-              SizedBox(height: deviceHeight * 0.243),
-              SizedBox(
-                width: deviceWidth * 0.85,
-                height: deviceHeight * 0.07,
-                child: CustomButton(
-                  btnLoading: true,
-                  onPressed: () async {
-                    //ログイン画面（トップ画面）へ遷移
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const TopPage()));
-                  },
-                  text: 'ログインする',
-                  colors: 0xFF74C13A,
-                ),
+            ),
+            SizedBox(height: deviceHeight * 0.3),
+            SizedBox(
+              width: deviceWidth * 0.85,
+              height: deviceHeight * 0.07,
+              child: CustomButton(
+                btnLoading: true,
+                onPressed: () async {
+                  //ログイン画面（トップ画面）へ遷移
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TopPage()));
+                },
+                text: 'ログインする',
+                colors: 0xFF74C13A,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
     }
